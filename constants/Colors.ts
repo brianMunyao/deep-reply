d4b0908575d8d095fa7cd22c021d3b999a1d8d09
brianMunyao <brianmunyao6@gmail.com>
2025-06-03 13:49:17 +0300

    feat: expand Colors constants with additional color properties for light and dark themes

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -7,20 +7,112 @@ const tintColorLight = '#0a7ea4';
 const tintColorDark = '#fff';
 
 export const Colors = {
-  light: {
-    text: '#11181C',
-    background: '#fff',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
-  },
+	light: {
+		white: '#FFF',
+		black: '#000',
+
+		text: '#11181C',
+		background: '#fff',
+		tint: tintColorLight,
+		icon: '#687076',
+		tabIconDefault: '#687076',
+		tabIconSelected: tintColorLight,
+
+		successDark: '#24B364',
+		success: '#28C76F',
+		successLight: '#53D28C',
+
+		dangerDark: '#E64449',
+		danger: '#FF4C51',
+		dangerLight: '#FF7074',
+
+		secondaryDark: '#737682',
+		secondary: '#808390',
+		secondaryLight: '#999CA6',
+
+		warningDark: '#E68F3C',
+		warning: '#FF9F43',
+		warningLight: '#FFB269',
+
+		infoDark: '#00A7BC',
+		info: '#00BAD1',
+		infoLight: '#33C8DA',
+
+		inputBackground: '#F2F2F2',
+		inputText: '#11181C',
+		inputTextPlaceholder: '#11181C',
+		inputDisabledBackground: '#E0E0E0',
+
+		buttonPrimaryBackground: '#000000',
+		buttonPrimaryText: '#FFFFFF',
+		buttonDisabledBackground: '#B0B0B0',
+		buttonDisabledText: '#7D7D7D',
+
+		dangerButtonBackground: '#D32F2F',
+		dangerButtonText: '#ffffff',
+
+		successButtonBackground: '#28C76F',
+		successButtonText: '#ffffff',
+
+		warningButtonBackground: '#FF9F43',
+		warningButtonText: '#000000',
+
+		cardBackground: '#F2F2F2',
+
+		separator: '#E1E4E8',
+	},
+	dark: {
+		white: '#FFF',
+		black: '#000',
+
+		text: '#ECEDEE',
+		background: '#151718',
+		tint: tintColorDark,
+		icon: '#9BA1A6',
+		tabIconDefault: '#9BA1A6',
+		tabIconSelected: tintColorDark,
+
+		successDark: '#24B364',
+		success: '#28C76F',
+		successLight: '#53D28C',
+
+		dangerDark: '#E64449',
+		danger: '#FF4C51',
+		dangerLight: '#FF7074',
+
+		secondaryDark: '#737682',
+		secondary: '#808390',
+		secondaryLight: '#999CA6',
+
+		warningDark: '#E68F3C',
+		warning: '#FF9F43',
+		warningLight: '#FFB269',
+
+		infoDark: '#00A7BC',
+		info: '#00BAD1',
+		infoLight: '#33C8DA',
+
+		inputBackground: '#2F2F2F',
+		inputText: '#fff',
+		inputTextPlaceholder: '#AAA',
+		inputDisabledBackground: '#4F4F4F',
+
+		buttonPrimaryBackground: '#F2F2F2',
+		buttonPrimaryText: '#000000',
+		buttonDisabledBackground: '#333333',
+		buttonDisabledText: '#7D7D7D',
+
+		dangerButtonBackground: '#B71C1C',
+		dangerButtonText: '#ffffff',
+
+		successButtonBackground: '#28C76F',
+		successButtonText: '#ffffff',
+
+		warningButtonBackground: '#FF9F43',
+		warningButtonText: '#000000',
+
+		cardBackground: '#2F2F2F',
+
+		separator: '#2C2C2C',
+	},
 };
